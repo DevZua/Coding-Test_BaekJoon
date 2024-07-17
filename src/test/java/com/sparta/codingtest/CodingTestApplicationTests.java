bdecca59bc3eca7853b795a8617786662508601d
Zua <gimzua0228@gmail.com>
2024-07-17 14:21:37 +0900

    first commit

---
+++ src/test/java/com/sparta/codingtest/CodingTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.codingtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
